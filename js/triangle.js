4b1fdc72b1fa70c72c72d7feba8312c09522eb12
imekenye <ismekenye@gmail.com>
2018-12-02 18:29:09 -0800

    Update javascript file

--- js/triangle.js
+++ js/triangle.js
@@ -3,6 +3,12 @@ function finalResult(){
     var sideB=parseInt(document.getElementById('sideB').value);
     var sideC=parseInt(document.getElementById('sideC').value);
     var answerResult = document.getElementById('answerResult');
+    if( isNaN(sideA) || isNaN(sideB) || isNaN(sideC)){
+      answerResult.textContent= "PLEASE ENTER NUMBER FOR ALL SIDES!";
+    }
+    if(sideA+sideB<=sideC || sideB+sideC<=sideA || sideA+sideC<=sideB){
+      answerResult.textContent="CANNOT FORM TRIANGLE!";
+    }
     if(sideA+sideB>sideC && sideB+sideC>sideA && sideA+sideB>sideC) {
       if (sideA==sideB&&sideA==sideC) {
         answerResult.textContent='Equilateral Triangle: All sides are equal!';
@@ -14,7 +20,5 @@ function finalResult(){
         answerResult.textContent="Scalene Triangle: No sides are equal!";
       }
     }
-     else {
-      answerResult.textContent="Cannot form a triangle!";
-    }
+     
     }
