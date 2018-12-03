568567e8f4a9aa3224dbb1576e9d70251da68438
imekenye <ismekenye@gmail.com>
2018-12-02 17:40:38 -0800

    update javascript file

--- js/triangle.js
+++ js/triangle.js
@@ -2,19 +2,19 @@ function finalResult(){
     var sideA=parseInt(document.getElementById('sideA').value);
     var sideB=parseInt(document.getElementById('sideB').value);
     var sideC=parseInt(document.getElementById('sideC').value);
-    var answerResult =document.getElementById('answerResult');
+    var answerResult = document.getElementById('answerResult');
     if(sideA+sideB>sideC && sideB+sideC>sideA && sideA+sideB>sideC) {
       if (sideA==sideB&&sideA==sideC) {
-        answerResult.textContent='Equilateral: All sides are equal!';
+        answerResult.textContent='Equilateral Triangle: All sides are equal!';
       }
       else if(sideA===sideB||sideB===sideC||sideA===sideC){
-        answerResult.textContent='Isosceles: Two sides are equal!'
+        answerResult.textContent='Isosceles Triangle: Two sides are equal!'
       }
       else{
-        answerResult.textContent="Scalene: No sides are equal!";
+        answerResult.textContent="Scalene Triangle: No sides are equal!";
       }
     }
      else {
-      answerResult.textContent="Not a triangle!";
+      answerResult.textContent="Cannot form a triangle!";
     }
     }
